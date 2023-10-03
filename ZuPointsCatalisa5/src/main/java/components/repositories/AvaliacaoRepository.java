a521e34ed3dca44dc380fdccfbfc478db53a337b
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-10-03 16:44:37 -0300

    feat: criado entidade avaliacao

---
+++ ZuPointsCatalisa5/src/main/java/components/repositories/AvaliacaoRepository.java
@@ -0,0 +1,12 @@
+package components.repositories;
+
+import components.models.Avaliacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
+    List<Avaliacao> findAllByDestinatario_Id(Long destinatarioId);
+}
