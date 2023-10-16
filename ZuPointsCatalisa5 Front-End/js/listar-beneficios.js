db3e61b68527664cf84337d2923549daebaba918
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-10-15 21:35:33 -0300

    subindo alterações

---
+++ ZuPointsCatalisa5 Front-End/js/listar-beneficios.js
@@ -0,0 +1,41 @@
+const listarProdutosBtn = document.getElementById("listarProdutosBtn");
+
+if (listarProdutosBtn) {
+    listarProdutosBtn.addEventListener("click", () => {
+        buscaTodosProdutos();
+    });
+}
+
+function buscaTodosProdutos() {
+    const token = localStorage.getItem('token');
+    const url = "http://localhost:8080/beneficios";
+
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}` 
+          }
+    })
+        .then((data) => {
+            return data.json();
+        })
+        .then((todosProdutos) => {
+            let data1 = "";
+            todosProdutos.forEach((values) => {
+                data1 += `
+            <tr>
+              <th scope="row">${values.id}</th>
+              <td>${values.nome}</td>
+              <td>${values.qtdPontosParaComprar}</td>
+              <td>${values.qtdDisponivel}</td>
+              <td>${values.valor}</td>
+            </tr>
+          `;
+            });
+            const tabela = document.getElementById("table-produtos").querySelector("tbody");
+            tabela.innerHTML = data1;
+        })
+        .catch((error) => {
+            console.error("Ocorreu um erro durante a solicitação:", error);
+        });
+}
